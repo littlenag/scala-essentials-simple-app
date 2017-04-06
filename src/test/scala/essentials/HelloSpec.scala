7a045f1e1926c0cba5dd44945715b513f0f931d3
Mark Kegel <mkegel@vast.com>
2017-04-05 19:36:38 -0500

    Placeholders for exercises

--- src/test/scala/essentials/HelloSpec.scala
+++ src/test/scala/essentials/HelloSpec.scala
@@ -10,4 +10,24 @@ class HelloSpec extends UnitSpec {
     }
   }
 
+  "Exercise 4" should {
+    "Write unit tests for the Map key/value reversing function you wrote in Exercise 2." in {
+      // Your code here!
+    }
+  }
+
+  "Exercise 5" should {
+    // Write a function that will transform a List[String] to a List[Double].
+    def toDoubles(stringList:List[String]) : List[Double] = Nil
+
+    "Write a unit test for the toDoubles function." in {
+      // Your code here!
+    }
+  }
+
+  "Exercise 6" should {
+    "Write a unit test for the code multiplication table printer you wrote in Exercise 1." in {
+      // Your code here!
+    }
+  }
 }
